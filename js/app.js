fee10e0aedc77a35893854df990199f1a97accec
taeng0204 <taeng02@icloud.com>
2023-01-26 23:12:51 +0900

    [2023.01.26] Modify css link in index.html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 const list = document.querySelectorAll('.list');
+const cssLink = document.getElementById("pageStyle");
 let selected = null;
+
+function changeStyle(url) {
+  if (cssLink) {
+    cssLink.href = "./css/" + url;
+  }
+}
 
 async function fetchHtmlAsText(url) {
   return await (await fetch("./html/" + url)).text();
@@ -9,7 +16,8 @@ async function importPage(event) {
   if (selected) selected.target.classList.remove("selected");
   selected = event;
   event.target.classList.add("selected");
-  document.querySelector('#main').innerHTML = await fetchHtmlAsText(event.target.id + '.html');
+  changeStyle(event.target.id + ".css")
+  document.querySelector('#main').innerHTML = await fetchHtmlAsText(event.target.id + ".html");
 }
 
 list.forEach((title) => (title.addEventListener("click", importPage)));
