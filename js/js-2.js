b777db0129769d93be71c9722d73d8d5afb8bb39
HBSPS <wiyeong79@naver.com>
2023-01-29 23:20:23 +0900

    feat: 조건문 예제 코드 추가

--- js/js-2.js
+++ js/js-2.js
@@ -15,14 +15,28 @@ function testFor() {
 function testBreak() {
   let i = 1;
   while (true) {
-    if ( i > 10 ) break;
+    if (i > 10) break;
 
-    if ( i % 2 !== 0 ) {
+    if (i % 2 !== 0) {
       i++;
       continue;
     }
-    
+
     console.log(i);
     i++;
+  }
+}
+
+function testIf() {
+  const inputElementValue = document.querySelector("#ifTest").value;
+
+  const i = Number(inputElementValue);
+
+  if (i % 2 === 0 && i >= 0) {
+    alert("짝수(또는 0) 입니다"); // i가 짝수인 경우 실행
+  } else if (i % 2 === 1 && i >= 0) {
+    alert("홀수 입니다"); // i가 홀수인 경우 실행 (모든 홀수를 2로 나눈 나머지는 1이다)
+  } else {
+    alert("양의 정수가 아닙니다"); // i가 양의 정수가 아닌 경우 실행
   }
 }
