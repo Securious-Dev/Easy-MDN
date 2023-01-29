33c1e8a0292877daa7b6ddaf2193881a5269ced7
HBSPS <wiyeong79@naver.com>
2023-01-29 22:43:51 +0900

    feat: break/continue 작성 완료

--- js/js-2.js
+++ js/js-2.js
@@ -11,3 +11,18 @@ function testFor() {
     console.log(i);
   }
 }
+
+function testBreak() {
+  let i = 1;
+  while (true) {
+    if ( i > 10 ) break;
+
+    if ( i % 2 !== 0 ) {
+      i++;
+      continue;
+    }
+    
+    console.log(i);
+    i++;
+  }
+}
