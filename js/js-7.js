64b8c3224257185b3bb023e4436e09c44e157876
HBSPS <wiyeong79@naver.com>
2023-01-31 16:54:32 +0900

    feat: API 예제 작성

--- js/js-7.js
+++ js/js-7.js
@@ -1,8 +1,9 @@
-function fetchTestWithoutAwait() {
-  return fetch('http://shibe.online/api/shibes?urls=true&httpsUrls=true').then(response => response.json());
+function fetchTest() {
+  const result = fetch('http://shibe.online/api/shibes?urls=true&httpsUrls=true')
+    .then(response => response.json())
+    .then(data => alertImgUrl(data));
 }
 
-function test() {
-  const data = fetchTestWithoutAwait();
-  console.log(data)
+function alertImgUrl(src) {
+  alert(src[0]);
 }
